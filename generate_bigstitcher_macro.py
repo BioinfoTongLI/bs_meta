855a3e29c0dda67c6af73c08ecef172e6292d448
Tong LI <tl10@sanger.ac.uk>
2020-11-23 14:24:03 +0000

    specify xml folder to decouple xml from data dir; append channel names to stitched image name

--- generate_bigstitcher_macro.py
+++ generate_bigstitcher_macro.py
@@ -6,6 +6,7 @@ class BigStitcherMacro:
     def __init__(self):
         self.img_dir = Path
         self.out_dir = Path
+        self.xml_folder = ""
         self.xml_file_name = 'dataset.xml'
         self.pattern = '1_{xxxxx}_Z001.tif'
 
@@ -49,7 +50,7 @@ class BigStitcherMacro:
         macro_template = self.estimate_stitch_param_macro_template
         formatted_macro = macro_template.format(img_dir=self.path_to_str(self.img_dir),
                                                 out_dir=self.path_to_str(self.out_dir),
-                                                path_to_xml_file=self.path_to_str(self.img_dir.joinpath(self.xml_file_name)),
+                                                path_to_xml_file=self.path_to_str(self.xml_folder.joinpath(self.xml_file_name)),
                                                 pattern=self.pattern,
                                                 num_tiles=self.make_range(self.num_tiles),
                                                 num_tiles_x=self.num_tiles_x,
@@ -79,36 +80,6 @@ class BigStitcherMacro:
         return str(path.absolute().as_posix())
 
     estimate_stitch_param_macro_template = """
-    // define dataset
-    run("BigStitcher",
-        "select=define" +
-        " define_dataset=[Manual Loader (Bioformats based)]" +
-        " project_filename={path_to_xml_file}" +
-        " multiple_timepoints=[NO (one time-point)]" +
-        " multiple_channels=[NO (one channel)]" +
-        " _____multiple_illumination_directions=[NO (one illumination direction)]" +
-        " multiple_angles=[NO (one angle)]" +
-        " multiple_tiles=[YES (one file per tile)]" +
-        " image_file_directory={img_dir}" +
-        " image_file_pattern={pattern}" +
-        " timepoints_=1" +
-        " tiles_={num_tiles}" +
-        " move_tiles_to_grid_(per_angle)?=[Move Tile to Grid (Macro-scriptable)]" +
-        " grid_type={tiling_mode}" +
-        " tiles_x={num_tiles_x}" +
-        " tiles_y={num_tiles_y}" +
-        " tiles_z=1" +
-        " overlap_x_(%)={overlap_x}" +
-        " overlap_y_(%)={overlap_y}" +
-        " overlap_z_(%)={overlap_z}" +
-        " calibration_type=[Same voxel-size for all views]" +
-        " calibration_definition=[User define voxel-size(s)]" +
-        " imglib2_data_container=[ArrayImg (faster)]" +
-        " pixel_distance_x={pixel_distance_x}" +
-        " pixel_distance_y={pixel_distance_y}" +
-        " pixel_distance_z={pixel_distance_z}" +
-        " pixel_unit=um");
-
     // calculate pairwise shifts
     run("Calculate pairwise shifts ...",
         "select={path_to_xml_file}" +
@@ -172,14 +143,14 @@ class FuseMacro:
     def replace_values_in_macro(self):
         macro_template = self.fuse_macro_template
         formatted_macro = macro_template.format(img_dir=self.path_to_str(self.img_dir),
-                                                path_to_xml_file=self.path_to_str(self.img_dir.joinpath(self.xml_file_name)),
+                                                path_to_xml_file=self.path_to_str(self.xml_folder.joinpath(self.xml_file_name)),
                                                 out_dir=self.path_to_str(self.out_dir)
                                                 )
         return formatted_macro
 
 
     def write_to_macro_file_in_channel_dir(self, img_dir: str, formatted_macro: str):
-        macro_file_path = img_dir.joinpath('fuse_only_macro.ijm')
+        macro_file_path = '%s_fuse_only_macro.ijm' %Path(self.xml_file_name).stem
         with open(macro_file_path, 'w') as f:
             f.write(formatted_macro)
         return macro_file_path
@@ -204,6 +175,7 @@ class FuseMacro:
         " interest_points_for_non_rigid=[-= Disable Non-Rigid =-]" +
         " blend produce=[Each timepoint & channel]" +
         " fused_image=[Save as (compressed) TIFF stacks]" +
-        " output_file_directory={out_dir}");
+        " output_file_directory={out_dir}" +
+        " filename_addition=DAPI");
 
     """
